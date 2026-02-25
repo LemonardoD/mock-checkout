0be01c9bfcfc020983ef32f7f088d56c5781fe85
David Hordiienko <shiraz0david@gmail.com>
2026-02-25 17:57:18 +0200

    refactor: update Checkout and index.astro for improved purchase submission flow

--- src/api/Checkout.ts
+++ src/api/Checkout.ts
@@ -4,6 +4,20 @@ class MockCheckout extends HttpClient {
   constructor() {
     super("https://dev-userspace-be-production.up.railway.app");
   }
+
+  submitPurchase = async (data: {
+    subscriptionId: string;
+    subscriptionType: "public";
+    subscriptionPeriod: "monthly" | "quarterly" | "yearly";
+    subscriptionStatus: "active";
+    subscriptionEndDate: string;
+    email: string;
+    password: string;
+    profileUsername: string;
+    amountUSD: string;
+    amount: string;
+    currency: string;
+  }) => this.post("/webhook-compat", { type: "init-purchase", data }, { headers: { "Content-Type": "application/json", Accept: "*/*" } });
 }
 
 export default MockCheckout;
